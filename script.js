2091d5fc81265dea5364aa17f61f13cda1869764
J-boardman <jaydeboardman@outlook.com>
2022-01-21 22:51:38 +1000

    finalized v2

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
 const sketcher = (() => {
-  //EVENT LISTENERS
-  rangeInput.addEventListener('input', () => render());
-  paintOn.addEventListener('input', () => render());
-  clear.addEventListener('click', () => render());
-  
   //DOM RENDER
   const render = () => {
-    let grids = rangeInput.value
+    let grids = rangeInput.value;
     display.textContent = `${grids} x ${grids}`;
-
+    
     while (container.firstChild) container.removeChild(container.lastChild);
-
+    
     for(let i=0;i<(grids*grids);i++){
       let div = container.appendChild(document.createElement('div'));
-
+      
       div.addEventListener(paintOn.value, () => color(div),true);
       div.addEventListener('click', () => checkEraser(div))
+      
       div.classList.add('grid');
       div.style['width'] = `${100/(grids)}%`;
       div.style['height'] = `${100/(grids)}%`;
     }
   }
+  //EVENT LISTENERS
+  clear.addEventListener('click', render);
+  [rangeInput,paintOn].forEach(x => x.addEventListener('input', render));
+
   //ADDITIONAL FUNCTIONS
   const color = (grid) => {
     eraser.checked ? null : grid.style['background-color'] = colorSelector.value;
   }
-
+  
   const checkEraser = (grid) =>{
     eraser.checked ? grid.style['background-color'] = 'white' : colorSelector.value
   }
+
   render();
 })();
