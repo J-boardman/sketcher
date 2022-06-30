7a48b709d2af247e38fcfef8d928e3f8440d0462
J-boardman <jaydeboardman@outlook.com>
2022-07-01 00:14:14 +1000

    Change painting style without re-render

--- script.js
+++ script.js
@@ -1,32 +1,47 @@
 const sketcher = (() => {
-  //DOM RENDER
-  const render = () => {
-    let grids = rangeInput.value;
+  // CACHE DOM
+  const gridDimensions = document.querySelector('#gridDimensions');
+  const paintStyle = document.querySelector('#paintStyle');
+  const clearBtn = document.querySelector('#clear')
+
+  // DOM RENDER
+  const render = (clearing) => {
+    let grids = gridDimensions.value;
     display.textContent = `${grids} x ${grids}`;
     
-    while (container.firstChild) container.removeChild(container.lastChild);
-    
-    for(let i=0;i<(grids*grids);i++){
-      let div = container.appendChild(document.createElement('div'));
-      
-      div.addEventListener(paintOn.value, () => color(div),true);
-      div.addEventListener('click', () => checkEraser(div))
-      
-      div.style.width = `${100/(grids)}%`;
-      div.style.height = `${100/(grids)}%`;
+    if(clearing){
+      while (container.firstChild) container.removeChild(container.lastChild);
+      for(let i=0; i<(grids*grids); i++){
+        let div = container.appendChild(document.createElement('div'));
+        div.setAttribute('data-grid', true);
+  
+        div.addEventListener(paintStyle.value, color);
+        div.addEventListener('click', () => checkEraser(div))
+        
+        div.style.width = `${100/(grids)}%`;
+        div.style.height = `${100/(grids)}%`;
+      }
+    } else {
+      const grids = document.querySelectorAll('[data-grid]');
+      grids.forEach(grid => {
+        grid.removeEventListener(paintStyle[paintStyle.value == 'click' ? 0 : 1].value, color)
+        grid.addEventListener(paintStyle.value, color);
+      })
     }
   }
+  
   //EVENT LISTENERS
-  clear.addEventListener('click', render);
-  [rangeInput,paintOn].forEach(x => x.addEventListener('input', render));
+  clearBtn.addEventListener('click', () => render(true));
+  gridDimensions.addEventListener('input', () => render(true));
+  paintStyle.addEventListener('input', () => render(false))
 
   //ADDITIONAL FUNCTIONS
-  const color = (grid) => {
-    eraser.checked ? null : grid.style['background-color'] = colorSelector.value;
+  const color = (e) => {
+    eraser.checked ? null : e.target.style['background-color'] = colorSelector.value;
   }
 
   const checkEraser = (grid) => {
     eraser.checked ? grid.style['background-color'] = 'white' : colorSelector.value
   }
-  render();
+  render(true);
 })();
